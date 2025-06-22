ce833d6daa7c46830792ab3417437eb874573be8
yan12182011 <tingyanc8@gmail.com>
2025-06-22 10:19:34 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -39,3 +39,36 @@ self.addEventListener("activate", event => {
     )
   );
 });
+const CACHE_NAME = "catgacha-v3"; // 每次都改版本
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/Ggggg.png",
+  "/card-R.png",
+  "/card-UR.png",
+  "/card-LSR.png",
+  "/card-SGR.png",
+  "/card-SC.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
